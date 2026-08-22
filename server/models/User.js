890e5efa891b5d644e1e336a2b8e313890222fec
Kunal More <95132438+TheDemon28@users.noreply.github.com>
2026-08-22 21:30:04 +0530

    fix: Update CSS styles for improved layout and responsiveness; enhance User model password hashing logic

--- server/models/User.js
+++ server/models/User.js
@@ -37,13 +37,14 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-userSchema.pre("save", async function () {
+userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
-    return;
+    return next();
   }
 
   const salt = await bcrypt.genSalt(12);
   this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 userSchema.methods.comparePassword = async function (enteredPassword) {
